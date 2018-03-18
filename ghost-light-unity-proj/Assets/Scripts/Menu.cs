dccc0dd04f454ea8070246a1e41917a470c998d3
mildmojo <mildmojo@users.noreply.github.com>
2018-03-18 01:44:13 -0700

    Improves StageManager game state usage.

--- ghost-light-unity-proj/Assets/Scripts/Menu.cs
+++ ghost-light-unity-proj/Assets/Scripts/Menu.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
+    public static Menu Instance;
     public Transform mainPanel;
     public Transform winPanel;
     public Transform losePanel;
@@ -11,19 +13,21 @@ public class Menu : MonoBehaviour {
     {
         get
         {
-            return (mainPanel.gameObject.activeInHierarchy 
+            return (mainPanel.gameObject.activeInHierarchy
                     || winPanel.gameObject.activeInHierarchy
                     || losePanel.gameObject.activeInHierarchy);
         }
     }
 
-    public static Menu Instance;
+    private CanvasGroup mainCanvas;
 
 	void Start () {
         Instance = this;
         mainPanel.gameObject.SetActive(true);
         winPanel.gameObject.SetActive(false);
         losePanel.gameObject.SetActive(false);
+        mainCanvas = GetComponent<CanvasGroup>();
+        mainCanvas.alpha = 1f;
     }
 
     void Update () {
@@ -48,29 +52,37 @@ public class Menu : MonoBehaviour {
     public void Win()
     {
         winPanel.gameObject.SetActive(true);
+        StageManager.instance.SetGameState(StageManager.State.SCENE_FINISH);
     }
 
     public void Continue()
     {
         winPanel.gameObject.SetActive(false);
         ScriptManager.instance.NewScene();
+        MeterManager.instance.Reset();
+        StageManager.instance.SetGameState(StageManager.State.PLAY);
     }
 
     public void Lose()
     {
         losePanel.gameObject.SetActive(true);
+        StageManager.instance.SetGameState(StageManager.State.SCENE_FINISH);
     }
 
     public void Retry()
     {
         losePanel.gameObject.SetActive(false);
+        MeterManager.instance.Reset();
         ScriptManager.instance.RetryScene();
+        StageManager.instance.SetGameState(StageManager.State.PLAY);
     }
 
     public void StartGame()
     {
         Time.timeScale = 1;
         mainPanel.gameObject.SetActive(false);
+        StageManager.instance.SetGameState(StageManager.State.PLAY);
+        MeterManager.instance.Reset();
     }
 
     public void QuitGame()
